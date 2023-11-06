6d4fc2d5c56039577e66bba8e5f2740c81b98259
Junaid bkiria <junaidbkibria@gmail.com>
2023-11-06 12:36:03 +0600

    warning removed

--- src/Pages/AnswerPage/AnswerForm.js
+++ src/Pages/AnswerPage/AnswerForm.js
@@ -1,10 +1,14 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import AlertModal from '../../Components/Alert'
 import { Box, Typography } from '@mui/material'
 
 const AnswerForm = () => {
     const [open, setOpen] = useState(false);
     const [message, setMessage] = useState("");
+
+    useEffect((() => {
+        setMessage("")
+    }),[])
     return (
         <Box sx={{ width: '90%', margin: '0 auto' }}>
             <Box sx={{ m: '20px 0' }}>
